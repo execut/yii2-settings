b64a6281ab93c1d8739042c869544a6fdbc1c725
eXeCUT notebook <sexecut@gmail.com>
2019-09-03 00:02:00 +0300

    Added model attributes to params

--- Component.php
+++ Component.php
@@ -26,14 +26,48 @@ class Component extends \yii\base\Component
             }
 
             $replacedParams = [];
-            foreach ($params as $key => $param) {
-                $replacedParams['{' . $key . '}'] = $param;
+            foreach ($params as $paramKey => $param) {
+                $replacedParams['{' . $paramKey . '}'] = $param;
             }
 
             $result = strtr($result, $replacedParams);
+            if ($key === 'search_catalog_message') {
+                $result = $this->replaceTemplate($result, $params);
+            }
             $this->cache[$key] = $result;
         }
 
         return $this->cache[$key];
+    }
+
+    protected function replaceTemplate($template, $models) {
+        $parts = explode('{', $template);
+        $result = '';
+        foreach ($parts as $part) {
+            if (strpos($part, '}') !== false) {
+                $subPart = explode('}', $part);
+                $modelKeyParts = explode('.', $subPart[0]);
+                $modelKey = $modelKeyParts[0];
+                if (!empty($models[$modelKey])) {
+                    $model = $models[$modelKey];
+                    unset($modelKeyParts[0]);
+                    $result .= $this->extractAttributeValue(implode('.', $modelKeyParts), $model);
+                }
+
+                $result .= $subPart[1];
+            } else {
+                $result .= $part;
+            }
+        }
+
+        return $result;
+    }
+
+    protected function extractAttributeValue($attribute, $model) {
+        try {
+            return ArrayHelper::getValue($model, $attribute);
+        } catch (\Exception $e) {
+            return '';
+        }
     }
 }
